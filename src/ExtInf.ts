1360b5a265f0926afdc4b4e487588c402f463187
Gunnar Cortes Heimisson <gnarr.c@gmail.com>
2019-05-30 18:16:41 +0000

    Added ExtInf.

---
+++ src/ExtInf.ts
@@ -0,0 +1,18 @@
+export default class ExtInf {
+  private duration!: number;
+  private title?: string;
+
+  public constructor(duration: number, title?: string) {
+    this.duration = duration;
+    this.title = title;
+  }
+
+  public print(version: number) {
+    const tags: string[] = [];
+    tags.push(`${version < 3 ? Math.round(this.duration) : this.duration}`);
+    if (this.title) {
+      tags.push(`[${this.title}]`);
+    }
+    return `#EXTINF:${tags.join(",")}`;
+  }
+}
